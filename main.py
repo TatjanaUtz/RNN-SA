95dd066cb42a691f5905f13927b5d97d36b3f5b2
TatjanaUtz <tatjana.utz@tum.de>
2019-02-19 08:20:30 +0100

    Added logging

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 
 Run this file for schedulability analysis with recurrent neural network (RNN).
 """
+from database import Database
+from logging_config import init_logging
+
+
+def main():
+    """Main function of project.
+
+    Run this method to perform schedulability analysis wit recurrent neural network (RNN).
+    """
+    logger = init_logging()  # initialize logging
+
+    my_database = Database()
+    tasksets, labels = my_database.read_all_tasksets()
+    logger.debug("Tasksets = %s", str(tasksets))
+    logger.debug("Labels = %s", str(labels))
+
+
+if __name__ == "__main__":
+    main()
