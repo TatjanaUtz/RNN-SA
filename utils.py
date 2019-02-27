1ce666eceb6dec3e2bffc8a933b8bfa344f4e2fb
TatjanaUtz <tatjana.utz@tum.de>
2019-02-27 09:58:09 +0100

    Added file for utility functions

---
+++ utils.py
@@ -0,0 +1,16 @@
+"""Utility functions helpfull within project."""
+
+from yaml import load
+from tensorflow.contrib.training import HParams
+
+class YParams(HParams):
+    def __init__(self, yaml_fn, config_name):
+        """Constructor."""
+        super().__init__()
+        with open(yaml_fn) as fp:
+            for k, v in load(fp)[config_name].items():
+                self.add_hparam(k, v)
+
+if __name__ == "__main__":
+    hparams = YParams('hparams.yaml', 'large_hidden')
+    print(hparams.num_hidden)  # print 1024
