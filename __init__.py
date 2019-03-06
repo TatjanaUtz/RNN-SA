f331ce3b823e20c1d6e98e1b2e81555075ab0f7a
TatjanaUtz <tatjana.utz@tum.de>
2019-03-06 13:21:47 +0100

    Added initialisation file for package

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""Makes helper libraries available in the RNN-SA package."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import util
