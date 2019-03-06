e0869f824cc3557171fc131a5396fd83ff02e390
TatjanaUtz <tatjana.utz@tum.de>
2019-03-06 11:34:22 +0100

    Addded summary helper function for TensorBoard

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 
 from yaml import load
 from tensorflow.contrib.training import HParams
+import tensorflow as tf
 
 class YParams(HParams):
     def __init__(self, yaml_fn, config_name):
@@ -12,6 +13,24 @@ class YParams(HParams):
             for k, v in load(fp)[config_name].items():
                 self.add_hparam(k, v)
                 self.dictionary[k] = v
+
+def variable_summaries(var):
+    """ This helper function from the TensorFlow documentation adds a few operations to log the
+    summaries.
+
+    Args:
+        var -- variable that should be summarized
+    """
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
 
 if __name__ == "__main__":
     hparams = YParams('hparams.yaml', 'large_hidden')
