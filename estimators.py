d794bbd94877af92603da5e6ae8dadb904e80217
TatjanaUtz <tatjana.utz@tum.de>
2019-03-08 07:41:49 +0100

    Added Keras LSTM model

--- estimators.py
+++ estimators.py
@@ -1,33 +1,53 @@
-"""Estimators for RNNs."""
+"""An Example of a custom Estimator.
+source: TensorFlow
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import numpy
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
 
-from database import Database
-from dataset import Dataset
+import logging_config
+from database_interface import Database
+from util import YParams
 
-TASK_ATTRIBUTES = ['priority', 'deadline', 'quota', 'caps', 'pkg', 'arg', 'cores', 'coreoffset',
-                   'criticaltime', 'period', 'numberOfJobs', 'offset']
+# TASK_ATTRIBUTES = ['priority', 'deadline', 'quota', 'caps', 'pkg', 'arg', 'cores', 'coreoffset',
+#                    'criticaltime', 'period', 'numberOfJobs', 'offset']
+TASK_ATTRIBUTES = ["Priority", "PKG", "Arg", "CRITICALTIME", "Period", "Number_of_Jobs"]
+
 TASK_PKGS = ['cond_mod', 'hey', 'pi', 'tumatmul']
 
 
-def train_input_fn(features, labels, batch_size, num_epochs):
-    """Import function for training."""
-    # convert the inputs to a Dataset
+def train_input_fn(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
     dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
 
-    # shuffle, repeat and batch the examples
-    return dataset.shuffle(1000).repeat(count=num_epochs).batch(batch_size)
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+
+    # Return the dataset.
+    return dataset
 
 
-def eval_input_fn(dataset):
-    """Import function for evaluation."""
-    # manipulate dataset, extracting the feature dict and the label
-    return feature_dict, label
+def eval_input_fn(features, labels, batch_size):
+    """An input function for training."""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
 
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
 
-def test_input_fn(dataset):
-    """Import function for test."""
-    # manipulate dataset, extracting the feature dict and the label
-    return feature_dict, label
+    # Return the dataset.
+    return dataset
 
 
 def define_feature_columns():
@@ -37,26 +57,31 @@ def define_feature_columns():
         list with defined feature columns
     """
     priority = tf.feature_column.numeric_column(key='priority', dtype=tf.int64)
-    deadline = tf.feature_column.numeric_column(key='deadline', dtype=tf.int64)
-    quota = tf.feature_column.numeric_column(key='quota', dtype=tf.int64)
-    caps = tf.feature_column.numeric_column(key='caps', dtype=tf.int64)
+    # deadline = tf.feature_column.numeric_column(key='deadline', dtype=tf.int64)
+    # quota = tf.feature_column.numeric_column(key='quota', dtype=tf.int64)
+    # caps = tf.feature_column.numeric_column(key='caps', dtype=tf.int64)
     pkg = tf.feature_column.categorical_column_with_vocabulary_list(key='pkg',
                                                                     vocabulary_list=TASK_PKGS)
     arg = tf.feature_column.numeric_column(key='arg', dtype=tf.int64)
-    cores = tf.feature_column.numeric_column(key='cores', dtype=tf.int64)
-    coreoffset = tf.feature_column.numeric_column(key='coreoffset', dtype=tf.int64)
+    # cores = tf.feature_column.numeric_column(key='cores', dtype=tf.int64)
+    # coreoffset = tf.feature_column.numeric_column(key='coreoffset', dtype=tf.int64)
     criticaltime = tf.feature_column.numeric_column(key='criticaltime', dtype=tf.int64)
     period = tf.feature_column.numeric_column(key='period', dtype=tf.int64)
     numberOfJobs = tf.feature_column.numeric_column(key='numberOfJobs', dtype=tf.int64)
-    offset = tf.feature_column.numeric_column(key='offset', dtype=tf.int64)
+    # offset = tf.feature_column.numeric_column(key='offset', dtype=tf.int64)
 
     # return list with defined feature columns
-    return [priority, deadline, quota, caps, pkg, arg, cores, coreoffset, criticaltime, period,
-            numberOfJobs, offset]
+    # return [priority, deadline, quota, caps, pkg, arg, cores, coreoffset, criticaltime, period,
+    #         numberOfJobs, offset]
+    return [priority, pkg, arg, criticaltime, period, numberOfJobs]
 
 
-def my_model_fn(features, labels, mode, params):
-    """Model function.
+def model_fn(features, labels, mode, params):
+    """Model function for RNN classifier.
+
+    The input is given to LSTM layers (as configured with params.num_layers and params.num_nodes).
+    The final state of the all LSTM layers are concatenated and fed to a fully connected layer to
+    obtain the final classification scores.
 
     Args:
         features -- batches of features returned from the input function
@@ -67,6 +92,7 @@ def my_model_fn(features, labels, mode, params):
     # use 'input_layer' to apply the feature columns
     net = tf.feature_column.input_layer(features, params['feature_columns'])
 
+    # TODO: Use RNN instead
     # Build the hidden layers, sized according to the 'hidden_units' param.
     for units in params['hidden_units']:
         net = tf.layers.dense(net, units=units, activation=tf.nn.relu)
@@ -84,7 +110,6 @@ def my_model_fn(features, labels, mode, params):
         }
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
-    # Compute evaluation.
     # Compute loss.
     loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
 
@@ -100,34 +125,54 @@ def my_model_fn(features, labels, mode, params):
         return tf.estimator.EstimatorSpec(
             mode, loss=loss, eval_metric_ops=metrics)
 
-    # compute training.
+    # create training op
+    assert mode == tf.estimator.ModeKeys.TRAIN
     optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
 
 if __name__ == "__main__":
-    # instantiate an estimator, passing the feature columns
-    feature_columns = define_feature_columns()
-    estimator = tf.estimator.Estimator(
-        model_fn=my_model_fn,
-        params={
-            'feature_columns': feature_columns,
-            'hidden_units': [10, 10],
-            'n_classes': 3,
-        })
+    # initialize logging
+    logging_config.init_logging()
 
-    # call a training, evaluation or inference method
-    estimator.train(input_fn=lambda: train_input_fn, steps=2000)
+    # get hyperparameters
+    hparams = YParams('hparams.yaml', 'LSTM')
 
-    eval_result = estimator.evaluate(input_fn=lambda: eval_input_fn())
-    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
-
-    # import data to memory
     my_database = Database()
-    tasksets, labels = my_database.read_all_tasksets_2D()
-    my_dataset = Dataset(tasksets, labels)
+    features, labels, _ = my_database.read_all_tasksets_preprocessed()
 
-    # pass this data to input function
-    batch_size = 100
-    train_input_fn(my_dataset.train.input, my_dataset.train.labels, batch_size)
+    X_train, X_test, y_train, y_test = train_test_split(features, labels)
+
+    # Example with keras
+    # create the model
+    model = Sequential()
+    model.add(LSTM(100))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_train, y_train, epochs=3, batch_size=64)
+    print(model.summary())
+
+    # Final evaluation of the model
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    print("Accuracy: %.2f%%" % (scores[1] * 100))
+
+    # # instantiate an estimator, passing the feature columns
+    # my_feature_columns = define_feature_columns()
+    #
+    # classifier = tf.estimator.Estimator(
+    #     model_fn=model_fn,
+    #     params={
+    #         'feature_columns': my_feature_columns,
+    #         'hidden_units': [10, 10],
+    #         'n_classes': 2,
+    #     })
+    #
+    # # Train the Model
+    # classifier.train(input_fn=lambda: train_input_fn(train_x, train_y, hparams.batch_size),
+    #                  steps=hparams.num_epochs)
+    #
+    # # evaluate the model
+    # eval_result = classifier.evaluate(
+    #     input_fn=lambda: eval_input_fn(test_x, test_y, hparams.batch_size))
+    # print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
