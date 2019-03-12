d1161818e46b24f46db81f840eb1345470828ba8
TatjanaUtz <tatjana.utz@tum.de>
2019-03-12 14:53:35 +0100

    Refractored code for usage of Keras LSTM model

--- main.py
+++ main.py
@@ -2,41 +2,47 @@
 
 Run this file for schedulability analysis with recurrent neural network (RNN).
 """
+
 import time
 
+from tensorflow import keras
 
 import logging_config
-import LSTM_models
-from dataset import Dataset
-from util import YParams
 from database_interface import Database
+from ml_models import LSTMModel
+from util import YParams, Config
 
 
 def main():
-    """Main function."""
+    """Main function of project RNN-SA."""
     # initialize logging
     logging_config.init_logging()
 
-    # get hyperparameters
-    hparams = YParams('hparams.yaml', 'LSTM')
+    # load data from the database
+    mydb = Database()
+    train_X, train_y, test_X, test_y = mydb.load_data()
 
-    # get the dataset from the database
-    my_dataset = Dataset(hparams=hparams)
+    # prepare the data: pad sequences to uniform length
+    train_X = keras.preprocessing.sequence.pad_sequences(train_X, padding='post', dtype=list)
+    test_X = keras.preprocessing.sequence.pad_sequences(test_X, padding='post', dtype=list)
 
-    # create LSTM model
-    lstm_model = LSTM_models.LSTM_Model(hparams)
+    # get hyperparameter and configuration parameter for LSTM
+    hparams_LSTM = YParams('hparams.yaml', 'LSTM')
+    config_LSTM = Config('config.yaml', 'LSTM')
 
+    # create a LSTM model
+    LSTM = LSTMModel(hparams_LSTM, config_LSTM)
+
+    # train the LSTM model
     start_time = time.time()
-    lstm_model.train(my_dataset.train)
+    LSTM.train(train_X, train_y)
     print("Time elapsed for training: ", time.time() - start_time)
 
+    # evaluate the LSTM model
     start_time = time.time()
-    lstm_model.test(my_dataset.test)
-    print("Time elapsed for test: ", time.time() - start_time)
-
+    LSTM.evaluate(test_X, test_y)
+    print("Time elapsed for evaluation: ", time.time() - start_time)
 
 
 if __name__ == "__main__":
     main()
-    # print("Main function of RNN-SA/main.py")
-
