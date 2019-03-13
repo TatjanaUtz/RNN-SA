28d52fc1379939d31835ae2b54f8b919f8c0fbd1
TatjanaUtz <tatjana.utz@tum.de>
2019-03-13 08:48:36 +0100

    Added create_dirs to create logging directories

--- util.py
+++ util.py
@@ -1,9 +1,13 @@
 """Utility functions helpfull within project."""
 
-from yaml import load
-from tensorflow.contrib.training import HParams
+import logging  # for logging
+import os
+import time # for getting the local time
+
 import tensorflow as tf
-import os, time
+from tensorflow.contrib.training import HParams
+from yaml import load
+
 
 class YParams(HParams):
     def __init__(self, yaml_fn, config_name):
@@ -14,6 +18,7 @@ class YParams(HParams):
             for k, v in load(fp)[config_name].items():
                 self.add_hparam(k, v)
                 self.dictionary[k] = v
+
 
 class Config(HParams):
     def __init__(self, yaml_fn, config_name):
@@ -26,14 +31,20 @@ class Config(HParams):
                 self.dictionary[k] = v
 
         # add tensorboard log dir
-        tensorboard_log_dir = os.path.join("experiments", time.strftime("%Y-%m-%d\\", time.localtime()), config_name, "logs\\")
+        tensorboard_log_dir = os.path.join("experiments",
+                                           time.strftime("%Y-%m-%d\\", time.localtime()),
+                                           config_name, "logs\\")
         self.add_hparam('tensorboard_log_dir', tensorboard_log_dir)
         self.dictionary['tensorboard_log_dir'] = tensorboard_log_dir
 
         # add checkpoint dir
-        checkpoint_dir = os.path.join("experiments", time.strftime("%Y-%m-%d\\", time.localtime()), config_name, "checkpoints\\")
+        checkpoint_dir = os.path.join("experiments", time.strftime("%Y-%m-%d\\", time.localtime()),
+                                      config_name, "checkpoints\\")
         self.add_hparam('checkpoint_dir', checkpoint_dir)
         self.dictionary['checkpoint_dir'] = checkpoint_dir
+
+        # create tensorboard and checkpoint dir
+        create_dirs([tensorboard_log_dir, checkpoint_dir])
 
 
 def variable_summaries(var):
@@ -52,6 +63,25 @@ def variable_summaries(var):
         tf.summary.scalar('max', tf.reduce_max(var))
         tf.summary.scalar('min', tf.reduce_min(var))
         tf.summary.histogram('histogram', var)
+
+
+def create_dirs(dirs):
+    """To create directories.
+
+    This function creates the given directories if these directories are not found.
+
+    Args:
+        dirs -- a list of directories
+    """
+    # create logger
+    logger = logging.getLogger('RNN-SA.util.create_dirs')
+
+    try:
+        for dir_ in dirs:
+            if not os.path.exists(dir_):
+                os.makedirs(dir_)
+    except Exception as exc:
+        logger.error("Creating directories error: %", exc)
 
 
 if __name__ == "__main__":
