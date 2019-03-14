7771a36811fe79f25855d91186f91821bf81cebb
TatjanaUtz <tatjana.utz@tum.de>
2019-03-14 11:46:18 +0100

    Deleted RNN and GRU models

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import time  # for measuring time
 
 import logging_config
 from database_interface import Database
-from ml_models import SimpleRNN, GRU, LSTM
+from ml_models import LSTM
 from util import YParams, Config
+import tensorflow as tf
 
 
 def main():
@@ -23,44 +24,6 @@ def main():
     # load data from the database
     mydb = Database()
     train_X, train_y, test_X, test_y = mydb.load_data()
-
-    # ----- SimpleRNN -----
-    # get hyperparameter and configuration parameter
-    hparams = YParams('hparams.yaml', 'SimpleRNN')
-    config = Config('config.yaml', 'SimpleRNN')
-
-    # create the model
-    simple_rnn = SimpleRNN(hparams, config)
-
-    # train the model
-    start_time = time.time()
-    simple_rnn.train(train_X, train_y)
-    logger.info("Time elapsed for training: %f", time.time() - start_time)
-
-    # evaluate the model
-    start_time = time.time()
-    loss, acc = simple_rnn.evaluate(test_X, test_y)
-    logger.info("Test loss = %f, test accuracy = %f", loss, acc)
-    logger.info("Time elapsed for evaluation: %f", time.time() - start_time)
-
-    # ----- GRU -----
-    # get hyperparameter and configuration parameter
-    hparams = YParams('hparams.yaml', 'GRU')
-    config = Config('config.yaml', 'GRU')
-
-    # create the model
-    gru = GRU(hparams, config)
-
-    # train the model
-    start_time = time.time()
-    gru.train(train_X, train_y)
-    logger.info("Time elapsed for training: %f", time.time() - start_time)
-
-    # evaluate the model
-    start_time = time.time()
-    loss, acc = gru.evaluate(test_X, test_y)
-    logger.info("Test loss = %f, test accuracy = %f", loss, acc)
-    logger.info("Time elapsed for evaluation: %f", time.time() - start_time)
 
     # ----- LSTM -----
     # get hyperparameter and configuration parameter
@@ -84,3 +47,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
