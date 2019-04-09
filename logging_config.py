9ddedb6c3dab1d40fae2234af1af9225f159d9c7
TatjanaUtz <tatjana.utz@gmail.com>
2019-04-09 14:16:29 +0200

    Added multi GPU support (basic version)

--- logging_config.py
+++ logging_config.py
@@ -1,21 +1,25 @@
 """Configurations for logging."""
 
 import logging
+import os
 
 
-
-def init_logging():
+def init_logging(db_dir, db_name):
     """Initializes logging.
 
     Configures logging. Error messages are logged to the 'error.log' file. Info messages are logged
     to the console. The results are save in a 'result_' log file.
+
+    Args:
+        db_dir -- directory of the database, used to create file for results
+        db_name -- name of the database, used to create file name for results
     """
     # create logger for traditional-SA project
     logger = logging.getLogger('RNN-SA')
     logger.setLevel(logging.INFO)
 
     # create file handler which logs error messages
-    log_file_handler = logging.FileHandler('error.log', mode='w+')
+    log_file_handler = logging.FileHandler(os.path.join(db_dir, 'error.log'), mode='w+')
     log_file_handler.setLevel(logging.ERROR)
 
     # create console handler with a lower log level (e.g debug or info)
