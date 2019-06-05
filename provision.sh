6b3d0a29a4f9c8f8e54cf81b38b3ac472eaf008c
TatjanaUtz <tatjana.utz@gmail.com>
2019-06-05 11:35:46 +0200

    Added vagrant support

---
+++ provision.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Add PPA
+sudo add-apt-repository ppa:jonathonf/python-3.6
+
+# Update all installed packages
+sudo apt-get update -qq
+
+# Install python 3.6
+sudo apt-get -y install python3.6
+
+# Install and upgrade pip
+sudo apt-get -y install python3-pip
+python3.6 -m pip install --upgrade pip
+
+# Install requirements
+pip install --user -r /vagrant/requirements.txt
